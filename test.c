807628874d9f002d881736cc7465c7d15849fd3f
Sensei Lee <sherlocksolution@gmail.com>
2021-03-30 16:06:41 +0200

    update

--- test.c
+++ test.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct node{
+  int data;
+  struct node* left;
+  struct node* mid;
+  struct node* right;
+};
+
+typedef struct node node;
+
 int main(){ return 0;}

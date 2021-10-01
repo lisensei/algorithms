66d8046c50eef99dd5974cfb0becfb58a06ca27a
Sensei Lee <sherlocksolution@gmail.com>
2021-10-01 17:03:21 +0200

    cifar10 cnn

---
+++ CL_CIFAR10_CNN.py
@@ -0,0 +1,346 @@
+import argparse
+import csv
+import os
+import random
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('agg')
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.utils
+import torch.nn.utils
+import torch.utils
+import torch.utils
+import torch.utils.data as Data
+import torchvision.models
+import torchvision.transforms as transform
+from torch.utils.tensorboard import SummaryWriter
+
+'''Argparse hyper parameters'''
+
+parser = argparse.ArgumentParser(description="Hyper parameters")
+parser.add_argument("-run_name", default=0)
+parser.add_argument("-sequence_name", default=0)
+parser.add_argument("-learning_rate", default=75e-4)
+parser.add_argument("-epoches", default=50)
+parser.add_argument("-batch_size", default=30)
+parser.add_argument("-noise_percent", default=0, type=int)
+parser.add_argument("-mean", default=0)
+parser.add_argument("-std", default=1)
+parser.add_argument("-classes", default=10)
+parser.add_argument("-curriculum", default=False)
+parser.add_argument("-samples", default=1)
+hp = parser.parse_args()
+
+'''This class is used to send data to tensorboard'''
+
+
+class DataVisualizer:
+    def __init__(self, log_path):
+        self.writer = SummaryWriter(log_path)
+
+    def addToTensorboard(self, figure, step):
+        self.writer.add_figure(tag="confusion matrix", figure=figure, global_step=step)
+
+    def plotMatrix(self, matrices):
+        figure = plt.figure(figsize=[10, 5])
+
+        for i in range(2):
+            matrix = matrices[i]
+            plt.subplot(1, 2, i + 1)
+            if i == 0:
+                plt.title("Training Set Confusion Matrix")
+            else:
+                plt.title("Test Set Confusion Matrix")
+            plt.imshow(matrix, interpolation="nearest", cmap=plt.get_cmap("Blues"))
+            plt.xticks(list(np.linspace(0, 9, 10, dtype=int)), ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+            plt.yticks(list(np.linspace(0, 9, 10)), ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+            for row in range(len(matrix[i])):
+                for column in range(len(matrix)):
+                    plt.annotate(str(int(matrix[row, column])), xy=(column, row),
+                                 horizontalalignment="center",
+                                 verticalalignment="center")
+            plt.xlabel("Predicted")
+            plt.ylabel("True")
+        return figure
+
+
+'''This class is used to process dataset,such as adding noise'''
+
+
+class DataProcessor:
+    def __init__(self, trainset_size=5 / 6, valset_size=1 / 6):
+
+        self.data_train = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform.Compose(
+            [
+                transform.ToTensor()
+            ]
+        ), download=False)
+        self.data_test = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform.Compose(
+            [
+                transform.ToTensor(),
+            ]
+        ), download=False)
+        self.data_test.data = torch.tensor(self.data_test.data).permute((0, 3, 1, 2))
+        max_samples = len(self.data_train.data)
+        self.train_samples = int(max_samples * trainset_size)
+        self.val_samples = int(max_samples * valset_size)
+        start_index = max_samples - self.val_samples
+        self.train_data = torch.clone(torch.tensor(self.data_train.data[0:self.train_samples])).permute((0, 3, 1, 2))
+        self.train_target = torch.clone(torch.tensor(self.data_train.targets[0:self.train_samples]))
+        self.validation_data = torch.clone(torch.tensor(self.data_train.data[start_index:])).permute((0, 3, 1, 2))
+        self.validation_target = torch.clone(torch.tensor(self.data_train.targets[start_index:]))
+        self.train_set = Data.DataLoader(
+            dataset=Data.TensorDataset(self.train_data.to(torch.float32),
+                                       self.train_target), batch_size=hp.batch_size, shuffle=True)
+        self.test_set = Data.DataLoader(
+            dataset=Data.TensorDataset(self.data_test.data.to(torch.float32),
+                                       torch.tensor(self.data_test.targets)), batch_size=hp.batch_size, shuffle=False)
+        self.validation_set = Data.DataLoader(
+            dataset=Data.TensorDataset(self.validation_data.to(torch.float32),
+                                       self.validation_target), batch_size=hp.batch_size, shuffle=True)
+
+    def repack(self):
+        self.train_set = Data.DataLoader(
+            dataset=Data.TensorDataset(self.train_data.to(torch.float32),
+                                       self.train_target), batch_size=hp.batch_size, shuffle=True)
+        self.test_set = Data.DataLoader(
+            dataset=Data.TensorDataset(self.data_test.data.to(torch.float32),
+                                       torch.tensor(self.data_test.targets)), batch_size=hp.batch_size, shuffle=False)
+
+    def getDataset(self, ):
+        return self.train_set, self.test_set
+
+    def addPepperNoise(self, percent):
+        self.resetDataset()
+        if percent >= 100:
+            percent = 100
+
+        for i, image in enumerate(self.train_data):
+            polluted_pixels = random.sample(range(0, 32 * 32), int(percent / 100 * 32 * 32))
+            for chn in range(3):
+                for i in range(len(polluted_pixels)):
+                    row = int(polluted_pixels[i] / 32)
+                    col = polluted_pixels[i] % 32
+                    if (row + col) % 2 == 0:
+                        image[chn][row][col] = 0
+                    else:
+                        image[chn][row][col] = 255.0
+        self.repack()
+
+    def resetDataset(self):
+        self.train_data = torch.clone(torch.tensor(self.data_train.data[0:self.train_samples])).permute((0, 3, 1, 2))
+        self.train_target = torch.clone(torch.tensor(self.data_train.targets[0:self.train_samples]))
+
+
+'''This class is used to make statistical calculations such as calculate F1 score'''
+
+
+class StatisticalAnalyzer:
+    def fonescore(self, matrix):
+        matrix_size = len(matrix)
+        parameters = torch.zeros((matrix_size, 3))
+        for i in range(matrix_size):
+            parameters[i][0] = matrix[i, i]
+            for j in range(matrix_size):
+                if j != i:
+                    parameters[i][1] += matrix[j, i]
+                    parameters[i][2] += matrix[i, j]
+
+        precision = 0
+        recall = 0
+        for i in range(matrix_size):
+            precision += parameters[i][0] / (parameters[i][0] + parameters[i][1]) / matrix_size
+            recall += parameters[i][0] / (parameters[i][0] + parameters[i][2]) / matrix_size
+
+        f1score = 2 * precision * recall / (precision + recall)
+        return f1score
+
+
+class CNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN, self).__init__()
+        self.num_classes = num_classes
+        self.covs = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=5, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=5, out_channels=8, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=8, out_channels=13, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=13),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.Conv2d(in_channels=13, out_channels=21, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=21, out_channels=34, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=34, out_channels=55, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=55),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=55, out_channels=89, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=89, out_channels=144, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=144, out_channels=243, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=243),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc = nn.Linear(in_features=4 * 4 * 243, out_features=120)
+        self.rl=nn.ReLU()
+        self.fcf = nn.Linear(in_features=120, out_features=self.num_classes)
+
+    def forward(self, x):
+        out = self.covs.forward(x).reshape(len(x), -1)
+        out = self.fc(out)
+        out = self.rl(out)
+        out = self.fcf(out)
+        return out
+
+
+torch.manual_seed(hp.run_name)
+random.seed(hp.run_name)
+processor = DataProcessor(hp.samples)
+if not hp.curriculum:
+    processor.addPepperNoise(hp.noise_percent)
+for runs in range(10):
+    hp.run_name = runs
+    running_noise = 0
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    log_path = "results/experiment_cifar10cnn/runs/sequence " + str(hp.sequence_name) + "/sequence " + str(
+        hp.sequence_name) + "_run " + str(
+        hp.run_name)
+
+    visualizer = DataVisualizer(log_path)
+    analyzer = StatisticalAnalyzer()
+    if hp.curriculum:
+        running_noise = 0
+        processor.addPepperNoise(running_noise)
+        postfix = "with curriculum.csv"
+    else:
+        running_noise = hp.noise_percent
+        postfix = "without curriculum.csv"
+
+    filename = "results/experiment_cifar10cnn/metrics/sequence " + str(hp.sequence_name) + "/sequence " + str(
+        hp.sequence_name) + "_" + "run " + str(
+        hp.run_name) + "_" + postfix
+    sequence_result = "results/experiment_cifar10cnn/metrics/sequence " + str(
+        hp.sequence_name) + "/summary of sequence " + str(
+        hp.sequence_name) + ".csv"
+
+    if not os.path.exists(filename):
+        with open(filename, 'w+', newline='') as csvfile:
+            fwrite = csv.writer(csvfile, delimiter=',')
+            fwrite.writerow(["Sequence", "Run Name", "Learning Rate", "Batch Size", "Noise Percent",
+                             "Curriculum", "Epoch", "Train Loss", "Train Accuracy", "Train F1",
+                             "Test Loss", "Test Accuracy", "Test F1", "Validation Accuracy", "Validation F1"])
+    if not os.path.exists(sequence_result):
+        with open(sequence_result, 'w+', newline='') as csvfile:
+            fwrite = csv.writer(csvfile, delimiter=',')
+            fwrite.writerow(
+                ["Sequence", "repeat_id", "Run Name", "Learning Rate", "Batch Size", "Noise Percent", "Curriculum",
+                 "Best Train Accuracy", "At Epoch", "Mean Train Accuracy",
+                 "Best Test Accuracy", "At Epoch", "Mean test accuracy", "Test F1", "Validation Accuracy",
+                 "Validation F1"])
+
+    '''ResNet with 10 classes'''
+    net = CNN(hp.classes)
+    if torch.cuda.is_available():
+        net.cuda()
+    loss = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=hp.learning_rate)
+
+    ''' metrics array holds metrics of each epoch
+        all_metrics holds all metrics from all epochs
+        at_epoch_test holds the index of the epoch that the
+        best test accuracy occurred
+    '''
+    all_metrics = list()
+
+    train_accuracies = torch.zeros((hp.epoches, 1))
+    test_accuracies = torch.zeros((hp.epoches, 1))
+    for epoch in range(hp.epoches):
+        metrics = dict()
+        metrics["validation_accuracy"] = 0
+        metrics["validation_f1score"] = 0
+        net.batch_size = hp.batch_size
+        if hp.curriculum:
+            running_noise += hp.noise_percent / hp.epoches
+            processor.addPepperNoise(running_noise)
+        confusion_matrix_train = torch.zeros((10, 10))
+        datasets = [processor.train_set, processor.test_set]
+        if epoch == hp.epoches - 1:
+            datasets.append(processor.validation_set)
+        for i, dataloader in enumerate(datasets):
+            train_loss = 0
+            train_correct = 0
+            confusion_matrix_train = torch.zeros((10, 10))
+            for j, (x, y) in enumerate(dataloader):
+                x = x.to(device)
+                y = y.to(device)
+                ypredict = net.forward(x).to(torch.float32)
+                indices = torch.argmax(ypredict, dim=1)
+                for row, column in zip(y, indices):
+                    confusion_matrix_train[row, column] += 1
+                train_correct += ((indices - y) == 0).sum()
+                batch_loss = loss.forward(ypredict, y)
+                train_loss += batch_loss
+                if dataloader == processor.train_set:
+                    batch_loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+            train_accuracy = train_correct / len(dataloader.dataset)
+            f1score_train = analyzer.fonescore(confusion_matrix_train)
+            if dataloader == processor.train_set:
+                metrics["train_loss"] = train_loss.item()
+                metrics["train_accuracy"] = train_accuracy.item()
+                metrics["train_f1score"] = f1score_train.item()
+                metrics["train_confusion_matrix"] = confusion_matrix_train.numpy()
+            elif dataloader == processor.test_set:
+                metrics["test_loss"] = train_loss.item()
+                metrics["test_accuracy"] = train_accuracy.item()
+                metrics["test_f1score"] = f1score_train.item()
+                metrics["test_confusion_matrix"] = confusion_matrix_train.numpy()
+            else:
+                metrics["validation_accuracy"] = train_accuracy.item()
+                metrics["validation_f1score"] = f1score_train.item()
+
+        # fig = visualizer.plotMatrix([metrics["train_confusion_matrix"], metrics["test_confusion_matrix"]])
+
+        # visualizer.addToTensorboard(fig, epoch)
+        plt.close("all")
+        all_metrics.append(metrics)
+        train_accuracies[epoch] = metrics["train_accuracy"]
+        test_accuracies[epoch] = metrics["test_accuracy"]
+        print(f'Epoch:{epoch},Run:{hp.run_name},current noise:{running_noise},'
+              f'Train loss:{metrics["train_loss"]:.4f},Train accuracy:{metrics["train_accuracy"]:.4f},'
+              f'train F1 score:{metrics["train_f1score"]:.4f},Test loss:{metrics["test_loss"]:.4f},'
+              f'Test accuracy:{metrics["test_accuracy"]:.4f},Test f1 score:{metrics["test_f1score"]:.4f}')
+
+        with open(filename, 'a', newline='') as csvfile:
+            fwrite = csv.writer(csvfile, delimiter=',')
+            fwrite.writerow(
+                [hp.sequence_name, hp.run_name, hp.learning_rate, hp.batch_size, running_noise, hp.curriculum, epoch,
+                 metrics["train_loss"],
+                 metrics["train_accuracy"],
+                 metrics["train_f1score"],
+                 metrics["test_loss"], metrics["test_accuracy"], metrics["test_f1score"],
+                 metrics["validation_accuracy"], metrics["validation_f1score"]])
+    at_epoch_train = torch.argmax(train_accuracies).item()
+    at_epoch_test = torch.argmax(test_accuracies).item()
+    mean_train_accuracy = torch.mean(train_accuracies).item()
+    mean_test_accuracy = torch.mean(test_accuracies).item()
+    with open(sequence_result, 'a', newline='') as csvfile:
+        fwrite = csv.writer(csvfile, delimiter=',')
+        fwrite.writerow(
+            [hp.sequence_name, runs, hp.run_name, hp.learning_rate, hp.batch_size, running_noise, hp.curriculum,
+             all_metrics[at_epoch_train]["train_accuracy"], at_epoch_train,
+             mean_train_accuracy, all_metrics[at_epoch_test]["test_accuracy"], str(at_epoch_test),
+             mean_test_accuracy, all_metrics[at_epoch_test]["test_f1score"], metrics["validation_accuracy"],
+             metrics["validation_f1score"]])
